49a8ff8ba38f6ab1f7cf41e87b3a957b75711419
unknown <yashkolnure58@gmail.com>
2026-03-26 02:07:25 +0530

    Initial commit

--- src/components/test/TestPanel.js
+++ src/components/test/TestPanel.js
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 
-const API = 'http://localhost:5000';
+const API = '';
 
 const tabStyle = (active) => ({
   flex: 1,
